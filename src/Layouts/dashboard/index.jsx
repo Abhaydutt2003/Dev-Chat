f59bcc7687de55660e6f448d95fc1305b5655b14
Abhay Dutt <dutt.abhay@gmail.com>
2024-02-16 19:03:53 +0530

    set up sideBar

--- src/Layouts/dashboard/index.jsx
+++ src/Layouts/dashboard/index.jsx
@@ -1,10 +1,23 @@
-import { Box, useTheme, Stack } from "@mui/material";
+import {
+  Box,
+  useTheme,
+  Stack,
+  IconButton,
+  Divider,
+  Avatar,
+} from "@mui/material";
+import {MaterialUISwitch} from '../../styled';
 import { Outlet } from "react-router-dom";
-
+import { faker } from "@faker-js/faker";
 import Logo from "../../assets/images/Dev.ico";
+import { Nav_Buttons } from "../../data/index";
+import { Gear } from "phosphor-react";
+import { useState } from "react";
 
 const DashboardLayout = () => {
   const theme = useTheme();
+  //state to know which element is currently selected
+  const [selected, setSelected] = useState(0);
   return (
     <>
       <Box
@@ -17,22 +30,56 @@ const DashboardLayout = () => {
         }}
       >
         <Stack
-          direction={"column"}
           alignItems={"center"}
-          sx={{ width: "100%" }}
+          justifyContent={"space-between"}
+          sx={{ width: "100%", height: "100%" }}
+          spacing={3}
         >
-          <Box
-            p={2}
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              width: "64px",
-              height: "64px",
-              borderRadius: "12px",
-              boxSizing: "border-box",
-            }}
-          >
-            <img src={Logo} alt="Dev-Chat-Image" />
-          </Box>
+          <Stack alignItems={"center"} spacing={3}>
+            <Box
+              p={2}
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                width: "64px",
+                height: "64px",
+                borderRadius: "12px",
+                boxSizing: "border-box",
+              }}
+            >
+              <img src={Logo} alt="Dev-Chat-Image" />
+            </Box>
+            {Nav_Buttons.map((ic) => {
+              const isSelected = ic.index === selected;
+              return (
+                <Box
+                  key={ic.index}
+                  p={1}
+                  sx={{
+                    boxSizing: "border-box",
+                    backgroundColor: isSelected
+                      ? theme.palette.primary.main
+                      : "",
+                    borderRadius: "12px",
+                  }}
+                >
+                  <IconButton
+                    onClick={() => setSelected(ic.index)}
+                    sx={{ color: isSelected ? "#fff" : "#000" }}
+                  >
+                    {ic.icon}
+                  </IconButton>
+                </Box>
+              );
+            })}
+            <Divider sx={{ width: "70px" }} />
+            <IconButton>
+              <Gear></Gear>
+            </IconButton>
+          </Stack>
+          <Stack alignItems={"center"} spacing={3}>
+            <MaterialUISwitch defaultChecked  />
+            <Avatar src={faker.image.avatar()}></Avatar>
+          </Stack>
         </Stack>
       </Box>
       <Outlet />
---
+++ src/data/index.jsx
@@ -0,0 +1,23 @@
+//could have made these in the respective files , but react does not enable fast refresh 
+//if the file does not export only a react component
+import {ChatCircleDots,Users,Phone} from 'phosphor-react';
+
+export const Nav_Buttons = [
+    {
+      index: 0,
+      icon: <ChatCircleDots />,
+    },
+    {
+      index: 1,
+      icon: <Users />,
+    },
+    {
+      index: 2,
+      icon: <Phone />,
+    },
+];
+
+
+
+
+
