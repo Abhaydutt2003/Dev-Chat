70839aa625ebf6907a9de54f2fd3ead39f4ba059
Abhay Dutt <dutt.abhay@gmail.com>
2024-03-12 06:47:20 +0530

    seprating middleware and controller logic

---
+++ Backend/middlewares/registerMiddleware.js
@@ -0,0 +1,49 @@
+const User = require("../Models/User");
+const filterObj = require("../utils/filterObj");
+const emailValidator = require("../utils/validateEmail");
+
+//next because this is a middleware
+const registerMiddleware = async (req, res, next) => {
+  const { firstName, lastName, email, pwd } = req.body;
+  if (!firstName || !lastName || !email || !pwd) {
+    return res.status(400).json({ message: "Please provide all the fields" });
+  }
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "pwd",
+    "email"
+  );
+  //validate the email
+  const isValid = emailValidator(email);
+  if(!isValid){
+    return res.status(400).json({
+        status:'success',
+        message:'Please enter a valid email'
+    });
+  }
+  //check if a verified user with the email exists
+  const existingUser = await User.findOne({ email: email });
+  if (existingUser && existingUser.verified) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email is already in use",
+    });
+  } else if (existingUser) {
+    //email is already there, just update any changed field
+    await User.findByIdAndUpdate({ email: email }, filteredBody, {
+      new: true,
+      runValidators: true,
+    });
+    next();
+  }else{
+    //first time 
+    const newUser = await User.create(filteredBody);
+    req.userId = newUser._id;
+    next();
+  }
+};
+
+//this middleware will check weather it is the first time that the user
+//is trying to signup to the website or not, ans will process accordingly
--- Backend/utils/filterObj.js
+++ Backend/utils/filterObj.js
@@ -1,5 +1,11 @@
 //there can be cases when the user has actually manipulated the form and 
 //passes feilds such as verified which can severly hurt the security
+/**
+ * 
+ * @param {Object} obj 
+ * @param  {...any} allowedFields 
+ * @returns {Object}
+ */
 const filterObj = (obj,...allowedFields)=>{
     const newObj = {};
     Object.keys(obj).forEach((el)=>{
